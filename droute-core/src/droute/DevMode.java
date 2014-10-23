e5bd8f1e23750fe8b00c7c0381daeafba264f48d
Alex Osborne <ato@meshy.org>
2014-10-23 14:00:16 +1100

    Add middleware to print exceptions

---
+++ droute-core/src/droute/DevMode.java
@@ -0,0 +1,26 @@
+package droute;
+
+import static droute.Response.response;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+public class DevMode {
+	public static Handler prettyErrors(Handler handler) {
+		return request -> {
+			try {
+				return handler.handle(request);
+			} catch (Throwable t) {
+				t.printStackTrace();
+				return response(500, renderError(t));
+			}
+		};
+	}
+
+	private static String renderError(Throwable t) {
+		StringWriter sw = new StringWriter();
+		t.printStackTrace(new PrintWriter(sw));
+		StringBuilder sb = new StringBuilder();
+		return "<!doctype html><h1>Internal Server Error</h1><pre>" + sw.toString() + "\n\n" + sb.toString();
+	}
+}
